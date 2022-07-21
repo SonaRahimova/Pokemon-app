40440779f989ba2e5f8e22dc562f303aed01d833
Sona Rahimova <sona.rahimova@se.experis.com>
2022-07-21 21:43:27 +0200

    stareted the project, setup the files, and added modules, components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { PokemonCataloguePage } from './pages/pokemon-catalogue/pokemon-catalogue.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
+// Decorator
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [   //Components
+    AppComponent,
+    LoginPage,
+    PokemonCataloguePage,
+    TrainerPage,
+    LoginFormComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [   //Modules
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiTrainers: "https://joso-noroff-api.herokuapp.com/users",
+  apiPokemons: "https://joso-noroff-api.herokuapp.com/trainers",
 };
