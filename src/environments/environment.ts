3ba928a9dfcea05407b831b712a10df75c2a9a9a
Sona Rahimova <sona.rahimova@se.experis.com>
2022-07-26 16:16:48 +0200

    removed our apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiTrainers: "https://joso-noroff-api.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0",
-  apiKey: "NnVsdwJd7UieOVAU3dKPQA=="
+  apiKey: ""
 };
 
 
