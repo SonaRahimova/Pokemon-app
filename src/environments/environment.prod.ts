7c23903cc2419c7a7d021016806d232cbdaf5f6c
Sona Rahimova <sona.rahimova@se.experis.com>
2022-07-26 16:23:55 +0200

    keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiTrainers: "https://joso-noroff-api.herokuapp.com/trainers",
   apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0",
-  apiKey: ""
+  apiKey: "NnVsdwJd7UieOVAU3dKPQA=="
 };
