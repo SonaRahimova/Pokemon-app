35ad91433885a1529b5ee9cbd05936903a7ad918
Sona Rahimova <sona.rahimova@se.experis.com>
2022-07-26 16:34:14 +0200

    Added environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiTrainers: "https://joso-noroff-api.herokuapp.com/trainers",
+  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0",
+  apiKey: "NnVsdwJd7UieOVAU3dKPQA=="
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiTrainers: "https://joso-noroff-api.herokuapp.com/trainers",
+  apiPokemons: "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0",
+  apiKey: "NnVsdwJd7UieOVAU3dKPQA=="
+};
+
+
